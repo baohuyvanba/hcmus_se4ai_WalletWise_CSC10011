33bc761dcac581811a67c0f2f69bede2e87f5090
ntthoa03 <145824268+ntthoa03@users.noreply.github.com>
2024-06-05 15:24:49 +0700

    data transaction

---
+++ app/src/main/java/com/finance/android/walletwise/model/Transaction/TransactionContainer.kt
@@ -0,0 +1,13 @@
+package com.finance.android.walletwise.model.Transaction
+
+import android.content.Context
+
+interface AppContainer {
+    val transactionRepository: TransactionRepository
+}
+
+class AppDataContainer(private val context: Context): AppContainer{
+    override val transactionRepository: TransactionRepository by lazy{
+        OfflineTransactionRepository(TransactionDatabase.getDatabase(context).transactionDao())
+    }
+}
