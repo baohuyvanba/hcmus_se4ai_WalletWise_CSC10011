4fc198e570ee9b2ca1a8b82b1325ebe738ef3b84
ntthoa03 <145824268+ntthoa03@users.noreply.github.com>
2024-06-21 20:15:37 +0700

    complete add and view list category.

---
+++ app/src/main/java/com/finance/android/walletwise/util/IconList.kt
@@ -0,0 +1,30 @@
+package com.finance.android.walletwise.util
+
+import com.finance.android.walletwise.R
+
+val categoriesList = listOf("Withdrawn", "Babycare", "Beauty", "Bills", "Birthday",
+    "Cafe", "Car", "Computer", "Donation", "Entertainment", "Fashion", "Food_folk",
+    "Fuel", "Grocery", "Health", "Home", "Income", "Insurance", "Leasure")
+
+val categoryIconsList = mapOf(
+//    "Withdrawn" to R.drawable.category_icon_bank,
+//    "Babycare" to R.drawable.category_icon_coffee,
+//    "Beauty" to R.drawable.category_icon_donate,
+//    "Bills" to R.drawable.category_icon_education,
+    "Birthday" to R.drawable.category_icon_food___drink,
+//    "Cafe" to R.drawable.category_icon_fuel,
+//    "Car" to R.drawable.category_icon_gift,
+//    "Computer" to R.drawable.category_icon_health,
+//    "Donation" to R.drawable.category_icon_home,
+    "Entertainment" to R.drawable.category_icon_laundry,
+    "Fashion" to R.drawable.category_icon_liquor,
+    "Food_folk" to R.drawable.category_icon_maintenance,
+    "Fuel" to R.drawable.category_icon_money,
+    "Grocery" to R.drawable.category_icon_party,
+    "Health" to R.drawable.category_icon_saving,
+    "Home" to R.drawable.category_icon_self_development,
+    "Income" to R.drawable.category_icon_shopping,
+    "Insurance" to R.drawable.category_icon_sport,
+    "Leasure" to R.drawable.category_icon_transportation,
+
+)
